19c5f3e8c904b751fad28fd341cf2c5468bb8285
Soma Froghyar <froghyar.soma@gmail.com>
2020-03-28 03:18:16 +0000

    init player

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+// player = {
+//     uniqueId: '1234'
+// }
 const rounds = [];
 const players = [];
 io.on("connection", socket => {
-
+    // create unique id for player who joined
+    var player = {
+        uniqueId: createUniqueId()
+    }
+    io.emit('player-entry', player);
+    console.log('Player ' + player.uniqueId + ' has connected');
+    players.push(player);
     const safeJoin = currentId => {
         socket.leave(previousId);
         socket.join(currentId);
@@ -13,6 +21,8 @@ io.on("connection", socket => {
     };
     io.emit('players', players);
 });
-
+function createUniqueId() {
+    return Math.floor(Math.random() * 100);
+}
 http.listen(4444);
 console.log("Let's go!");
