29896b92b9fe9b1b38d357e20a0b0988007f7d16
Soma Froghyar <froghyar.soma@gmail.com>
2020-03-29 19:09:27 +0100

    lobby connection added

--- src/app.js
+++ src/app.js
@@ -1,28 +1,40 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var fs = require('fs');
 // player = {
 //     uniqueId: '1234'
 // }
+
+http.listen(4444, function () {
+    console.log("Let's go!");
+});
+
 const rounds = [];
 const players = [];
+var numUsers = 0;
 io.on("connection", socket => {
-    // create unique id for player who joined
-    var player = {
-        uniqueId: createUniqueId()
-    }
-    io.emit('player-entry', player);
-    console.log('Player ' + player.uniqueId + ' has connected');
-    players.push(player);
-    const safeJoin = currentId => {
-        socket.leave(previousId);
-        socket.join(currentId);
-        previousId = currentId;
-    };
-    io.emit('players', players);
+    var addedUser = false;
+    socket.on('landing', () => {
+            console.log('user count: ', numUsers);
+            socket.emit('playerCount', numUsers);
+    });
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) => {
+        if (addedUser) {
+            return;
+        };
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', numUsers);
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+        console.log('Added ' + username + ' to the game');
+    });
 });
-function createUniqueId() {
-    return Math.floor(Math.random() * 100);
-}
-http.listen(4444);
-console.log("Let's go!");
+
